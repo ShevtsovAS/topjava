d4dcf4ffa76a4319c6da6750137bdaf508b52edf
ashevtsov <Aleksandr.Shevtsov@excelian.com>
2018-08-07 18:00:05 +0300

    HW05

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -3,5 +3,16 @@ package ru.javawebinar.topjava.repository.datajpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    Meal findFirstByIdAndUser_Id(int id, int userId);
+
+    boolean deleteByIdAndUser_Id(int id, int userId);
+
+    List<Meal> findAllByUser_IdOrderByDateTimeDesc(int userId);
+
+    List<Meal> findAllByUser_IdAndDateTimeBetweenOrderByDateTimeDesc(int userId, LocalDateTime startDate, LocalDateTime endDate);
 }
